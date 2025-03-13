60c2484efe24b8dea248842bd2bbeb83eac9cba4
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-03-13 08:57:36 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,20 @@ sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 os.system("rm -r freenove_Kit")
 os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Three-wheeled_Smart_Car_Kit_for_Raspberry_Pi freenove_Kit")
 
+# 删除文件夹
+os.system("rm -r _static")
+os.system("rm -r _templates")
+os.system("rm freenove-logo.png")
+
 # 克隆所需资料到freenove_css文件夹中
-os.system("git clone --depth 1 https://github.com/Vegetable-SYC/templete-css freenove_css")
+os.system("git clone https://github.com/Vegetable-SYC/templete-css freenove_css")
 
 # 复制网页样式到对应位置
 os.system("cp -r freenove_css/_static .")
 os.system("cp -r freenove_css/_templates .")
+
+# 复制网页样式到对应位置
+os.system("cp freenove_css/freenove-logo.png .")
 
 # 删除文件夹
 os.system("rm -r freenove_css")
